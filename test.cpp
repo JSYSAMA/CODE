1bdd251c91fddea6cc168d6178737ffa82b88ccd
JSYSAMA <jiangsiyuan2009@yeah.net>
2020-11-18 19:08:49 +0800

    2020/11/18 STL

--- test.cpp
+++ test.cpp
@@ -1,8 +1,15 @@
 //just a git test
 #include<iostream>
+#include<vector>
+#include<algorithm>
+#include<functional>
+
 using namespace std;
+
 int main()
 {
-    cout<<"hello,world"<<endl;
+    int ia[6]={27,210,12,47,109,83};
+    vector<int,allocator<int>> vi(ia,ia+6);
+    cout<<count_if(vi.begin(),vi.end(),not1(bind(less<int>(),placeholders::_1,40)));
     return 0;
 }
